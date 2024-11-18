ca8e3d4178b4a27872ac05eb30d7b910ccf9f96c
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-18 19:59:55 -0400

    inserido segunda interação usuário, senha inválida

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ titulo.style.color = "blue"
 titulo.style.fontSize = "2.5rem"
 
 //Primeira interação do usuário
-// Digitado nome de usuário inválido
+//Digitado nome de usuário inválido
 let loginUsuario = document.getElementById("login-usuario")
 let errorTextUsuario = document.querySelector(".error-text")
 console.log(errorTextUsuario)
 
 loginUsuario.classList.add("error")
 errorTextUsuario.classList.add("visible")
+
+//Segunda interação do usuário
+//Digitado nome de usuário válido, mas com senha incorreta
+let loginSenha = document.getElementById("login-senha")
+let errorTextSenha = document.querySelector(".error-text-senha")
+
+loginSenha.classList.add("error")
+errorTextSenha.classList.add("visible")
