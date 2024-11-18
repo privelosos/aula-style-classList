a0b17f07fb5380ec7116f7ea46b0db3ca45143a3
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-18 19:51:30 -0400

    inserido lista de classe

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log(titulo)
 
 titulo.style.color = "blue"
 titulo.style.fontSize = "2.5rem"
+
+//Primeira interação do usuário
+// Digitado nome de usuário inválido
+let loginUsuario = document.getElementById("login-usuario")
+let errorTextUsuario = document.querySelector(".error-text")
+console.log(errorTextUsuario)
+
+loginUsuario.classList.add("error")
+errorTextUsuario.classList.add("visible")
